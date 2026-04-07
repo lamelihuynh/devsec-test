acc827483cd007552b48bf72cd966b889c5610af
lamelihuynh <gianglinh271@gmail.com>
2026-04-07 15:39:45 +0700

    Commit version 8

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./styles.css";
-API_KEY = KAKAKAKKAKAKKA
-DB_PASSWORD=my_super_secret_pwd
 import Tetris from "./components/Tetris";
 
 export default function App() {
